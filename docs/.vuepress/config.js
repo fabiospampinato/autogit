a4786f371d6642b7e8db14ecd68d7680472181a2
Fabio Spampinato <spampinabio@gmail.com>
2018-10-12 00:19:51 +0200

    Simplified docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,27 +9,18 @@ const config = {
     repo: 'fabiospampinato/autogit',
     docsRepo: 'fabiospampinato/autogit',
     docsDir: 'docs',
-    editLinks: true,
-    editLinkText: 'Help us improve this page!',
-    lastUpdated: true,
     serviceWorker: {
       updatePopup: true
     },
     displayAllHeaders: true,
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Configuration', link: '/configuration' },
-      { text: 'Commands', link: '/commands' },
-      { text: 'Plugins', link: '/plugins' }
-    ],
     sidebar: [
-      ['/', 'Home'],
+      ['/', 'Autogit'],
       '/configuration',
       '/commands',
       '/plugins'
     ]
   }
-}
+};
 
 /* EXPORT */
 
